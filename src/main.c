881a17c8daada3ee1a16067f4cc433f62c75dd4c
flamfrosticboio <105685349+flamfrosticboio@users.noreply.github.com>
2025-03-15 17:26:51 +0800

    fixed your damn stupid condition

--- src/main.c
+++ src/main.c
@@ -19,9 +19,9 @@ int main()
   {
     printf("Enter text: ");
     read_line(cmdline);
-
-    for (int i = 0; current_arg != NULL; i++)
-    {
+    
+    int i = 0;
+    do {
       current_arg = get_arg(cmdline);
 
       if (current_arg == NULL)
@@ -30,17 +30,20 @@ int main()
       }
 
       printf("[%d]:\t%s\n", i, current_arg);
-    }
 
-    printf("$PATH raw: %s\n", getenv("PATH"));
-    printf("$PATH dump:\n");
-    curr_path = get_path();
-    for (int i = 0; curr_path != NULL; i++)
-    {
-      printf("%d\t:%s\n", i, curr_path);
-      curr_path = get_path();
-    }
+      i++;
+    } while (current_arg != NULL);
 
+    // printf("$PATH raw: %s\n", getenv("PATH"));
+    // printf("$PATH dump:\n");
+    // curr_path = get_path();
+    // for (int i = 0; curr_path != NULL; i++)
+    // {
+    //   printf("%d\t:%s\n", i, curr_path);
+    //   curr_path = get_path();
+    // }
+
+    cmd_tok_free(cmdline);
   } while (1);
 
   cmd_free(cmdline);
