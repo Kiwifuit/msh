64d87d97651d9b20eacdc0764be234674b395381
Kiwifuit <75169058+Kiwifuit@users.noreply.github.com>
2025-03-15 16:31:40 +0800

    chore: save on format

--- src/tokenize.c
+++ src/tokenize.c
@@ -3,44 +3,51 @@
 #include <stdbool.h>
 #include <string.h>
 
-char *tokenize(char *str, const char *delim) {
-    static char *next_token = NULL;
-    if (str != NULL) {
-        next_token = str;  
+char *tokenize(char *str, const char *delim)
+{
+  static char *next_token = NULL;
+  if (str != NULL)
+  {
+    next_token = str;
+  }
+  if (next_token == NULL || *next_token == '\0')
+  {
+    return NULL;
+  }
+
+  bool in_quote = false;
+  char *token_start = next_token;
+
+  while (*next_token && strchr(delim, *next_token))
+  {
+    next_token++;
+  }
+
+  if (*next_token == '\0')
+  {
+    return NULL;
+  }
+
+  token_start = next_token;
+
+  while (*next_token)
+  {
+    if (*next_token == '"')
+    {
+      in_quote = !in_quote;
     }
-    if (next_token == NULL || *next_token == '\0') {
-        return NULL;  
+    else if (!in_quote && strchr(delim, *next_token))
+    {
+      break;
     }
+    next_token++;
+  }
 
-    bool in_quote = false;
-    char *token_start = next_token;
+  if (*next_token)
+  {
+    *next_token = '\0';
+    next_token++;
+  }
 
-    
-    while (*next_token && strchr(delim, *next_token)) {
-        next_token++;
-    }
-
-    if (*next_token == '\0') {
-        return NULL;  
-    }
-
-    token_start = next_token;  
-
-    
-    while (*next_token) {
-        if (*next_token == '"') {
-            in_quote = !in_quote;  
-        } else if (!in_quote && strchr(delim, *next_token)) {
-            break;  
-        }
-        next_token++;
-    }
-
-    
-    if (*next_token) {
-        *next_token = '\0';
-        next_token++;
-    }
-
-    return token_start;
+  return token_start;
 }
