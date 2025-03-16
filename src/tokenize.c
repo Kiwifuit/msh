581933bea04c9b5dfec4522b3a996b6ebdfd46c7
Kiwifuit <75169058+Kiwifuit@users.noreply.github.com>
2025-03-16 17:01:14 +0800

    feat: split `tokenize` into appropriate functions

--- src/tokenize.c
+++ src/tokenize.c
@@ -3,51 +3,68 @@
 #include <stdbool.h>
 #include <string.h>
 
+static char *skip_delim(char *s, const char *delim)
+{
+  while (*s && strchr(delim, *s))
+  {
+    s++;
+  }
+  return s;
+}
+
+static char *find_tokend(char *s, const char *delim)
+{
+  bool quoted = false;
+
+  while (*s)
+  {
+    if (*s == '\"')
+      quoted = !quoted;
+    else if (!quoted && strchr(delim, *s))
+      break;
+
+    s++;
+  }
+
+  return s;
+}
+
+static char *strip_quotes(char *tok)
+{
+  size_t tok_len = strlen(tok);
+  if (tok_len >= 2 && tok[0] == '\"' && tok[tok_len - 1] == '\"')
+  {
+    tok[tok_len - 1] = '\0';
+    return tok + 1;
+  }
+  return tok;
+}
+
 char *tokenize(char *str, const char *delim)
 {
   static char *next_token = NULL;
   if (str != NULL)
-  {
     next_token = str;
-  }
-  if (next_token == NULL || *next_token == '\0')
-  {
+
+  if (next_token == NULL)
     return NULL;
-  }
 
-  bool in_quote = false;
-  char *token_start = next_token;
-
-  while (*next_token && strchr(delim, *next_token))
-  {
-    next_token++;
-  }
-
+  next_token = skip_delim(next_token, delim);
   if (*next_token == '\0')
-  {
     return NULL;
-  }
 
-  token_start = next_token;
+  char *token_start = next_token;
+  char *token_end = find_tokend(next_token, delim);
 
-  while (*next_token)
+  if (*token_end)
   {
-    if (*next_token == '"')
-    {
-      in_quote = !in_quote;
-    }
-    else if (!in_quote && strchr(delim, *next_token))
-    {
-      break;
-    }
-    next_token++;
+    *token_end = '\0';
+    next_token = token_end + 1;
   }
-
-  if (*next_token)
+  else
   {
-    *next_token = '\0';
-    next_token++;
+    next_token = NULL;
   }
 
-  return token_start;
+  return strip_quotes(token_start);
 }
